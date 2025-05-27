3a3406350f96e70a0d790012a40eab528e343e5a
Shout2010 <baronZigzag@yandex.ru>
2025-05-27 17:25:28 +0500

    hw-2

---
+++ js/hv-2.js
@@ -0,0 +1,66 @@
+// задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// задание 2
+
+let release = 2007;
+alert(`Дата выпуска IPhone: ${release}`);
+
+// задание 3
+
+let creatorJs = "Brendan Eich";
+alert("Создатель JS: " + creatorJs);
+
+// задание 4
+
+let b = 10;
+let c = 2;
+alert(`Сумма чисел 10 и 2 равна: ${b+c}`);
+alert(`Разность чисел 10 и 2 равна: ${b-c}`);
+alert(`Произведение чисел 10 и 2 равна: ${b*c}`);
+alert(`Частное чисел 10 и 2 равно: ${b/c}`);
+
+// задание 5
+
+let result = 2**5;
+alert(`Два в пятой степени: ${result}`);
+
+// задание 6
+
+let a = 9;
+let b = 2;
+alert(`Остаток от деления 9 на 2 равен ${a%b}`);
+
+// задание 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+
+// задание 8
+
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
+// задание 9
+
+let user = {
+    name:'Vasya',
+    age: 32,
+    isAdmin: false
+}
+
+// задание 10
+
+let userName = prompt("Введите ваше имя: ");
+alert(`Привет, ${userName}!`);
