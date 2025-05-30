6ae232a76f4c8437a5d53e634aad4628cf1dc13a
Shout2010 <baronZigzag@yandex.ru>
2025-05-30 13:20:00 +0500

    домашнее задание 5

---
+++ js/searchNumber.js
@@ -0,0 +1,26 @@
+const randomGame = () =>{
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
+    
+    do{
+        let userNumber = prompt('Угадай число от 1 до 100',"Нажми отмена что бы закочнить игру");
+
+        if (userNumber === null){
+            alert(`Рано сдался, а число было ${randomNumber}`);
+            break;
+        }
+
+        if (userNumber < 1 || userNumber > 100 || isNaN(userNumber) || userNumber == 0){
+            alert('Мы в игру играем, или дурью маемся???');
+            continue;
+        }
+
+        if (userNumber > randomNumber){
+            alert("Попробуй число поменьше");
+        } else if (userNumber < randomNumber){
+            alert("Попробуй число побольше");
+        } else if (userNumber == randomNumber){
+            alert(`Поздравлем ты угадал. Это было число ${userNumber}`);
+            break;
+        }
+    } while (true)
+}
